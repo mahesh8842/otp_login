ba4d4fe65515c8ece316c01938c87d1cf7a287dc
mahesh8842 <maheshkumar88425@gmail.com>
2022-12-24 20:51:51 +0530

    first commit

---
+++ otp/home/helpers.py
@@ -0,0 +1,17 @@
+import random
+import requests
+
+from django.conf import settings
+
+def send_otp_to_mobile(mobile):
+    try:
+        otp = random.randint(100000,999999)
+        url = f'https://2factor.in/API/V1/{settings.API_KEY}/SMS/{mobile}/{otp}'
+        #curl --location --request GET 'https://2factor.in/API/V1/XXXX-XXXX-XXXX-XXXX-XXXX/SMS/+919999999999/12345/OTP1'
+        # response = requests.get(url)
+        # print(response)
+        print(otp)
+        return otp
+    except Exception as e:
+        print(e)
+        return None
